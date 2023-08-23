6e34ed7c1889ea3f246a6218ef60e0afc2c330b0
Owdea <ondrejpel@seznam.cz>
2023-08-23 17:34:53 +0200

    new indextx.js

--- indexts.js
+++ indexts.js
@@ -24,6 +24,7 @@ funkce:
 /*
      todo: Přepsat do Typescriptu a vymyslet jak vynechat this.*
 */
+//todo: 
 var buttons = document.querySelectorAll(".button");
 var buttonSelected = document.getElementsByClassName("active");
 var reset = document.querySelector("#main-right #button-reset");
@@ -34,105 +35,134 @@ var bill = document.querySelector("main #main-left #bill .input");
 var numberOfPeople = document.querySelector("main #main-left-bottom #number-people .input");
 var errorMessage = document.querySelector(".if-zero-number");
 var customTip = document.querySelector(".custom");
-customTip.addEventListener("click", calculateCustomTip);
+customTip === null || customTip === void 0 ? void 0 : customTip.addEventListener("click", calculateCustomTip); //optional chaining
 buttons.forEach(function (button) {
     button.addEventListener("click", function () { return calculateTip(button); });
 });
 // const mujListener = (event) => {event.target.innerText = "Ahoj"};
 // function addEventListener(eventType: String, listener: (event: HTMLEvent) => void)
-reset.addEventListener("click", resetAll);
-bill.oninput = function (event) {
+reset === null || reset === void 0 ? void 0 : reset.addEventListener("click", resetAll);
+bill === null || bill === void 0 ? void 0 : bill.addEventListener("input", function () {
     dealWithResetButton();
-    if (customTip.value !== "" && (numberOfPeople.value !== "" || parseInt(numberOfPeople.value) > 0)) {
+    if ((customTip === null || customTip === void 0 ? void 0 : customTip.value) !== "" && ((numberOfPeople === null || numberOfPeople === void 0 ? void 0 : numberOfPeople.value) !== "" || parseInt(numberOfPeople.value) > 0)) {
         calculate();
     }
-};
-customTip.oninput = function (event) {
+});
+/*
+customTip.oninput = function(event) {
     dealWithResetButton();
-    if ((bill.value !== "" || parseFloat(bill.value) < 0) && (numberOfPeople.value !== "" || parseInt(numberOfPeople.value) > 0)) {
+
+    if((bill.value !== "" || parseFloat(bill.value) < 0) && (numberOfPeople.value !== "" || parseInt(numberOfPeople.value) > 0)) {
+        calculate()
+    }
+}
+*/
+customTip === null || customTip === void 0 ? void 0 : customTip.addEventListener("input", function (event) {
+    dealWithResetButton();
+    if (((bill === null || bill === void 0 ? void 0 : bill.value) !== "" || parseFloat(bill.value) < 0) && ((numberOfPeople === null || numberOfPeople === void 0 ? void 0 : numberOfPeople.value) !== "" || parseInt(numberOfPeople.value) > 0)) {
         calculate();
     }
-};
-numberOfPeople.oninput = function (event) {
+});
+numberOfPeople === null || numberOfPeople === void 0 ? void 0 : numberOfPeople.addEventListener("input", function (event) {
     dealWithResetButton();
     if (parseFloat(numberOfPeople.value) <= 0 || numberOfPeople.value === "") {
-        errorMessage.innerText = "can't be zero";
-        errorMessage.style.color = 'red';
+        if (errorMessage) {
+            errorMessage.innerText = "can't be zero";
+            errorMessage.style.color = 'red';
+        }
         numberOfPeople.style.borderColor = 'red';
-        resultTip.innerText = '----';
-        resultTotal.innerText = '----';
+        if (resultTip)
+            resultTip.innerText = '----';
+        if (resultTotal)
+            resultTotal.innerText = '----';
     }
     else {
-        errorMessage.innerText = "";
+        if (errorMessage)
+            errorMessage.innerText = "";
         numberOfPeople.style.borderColor = "";
         calculate();
     }
-};
+});
 function calculate() {
     var tipPercentage;
     if (buttonSelected.length == 0) {
         tipPercentage = 0;
     }
     else {
-        if (customTip.classList.contains("active")) {
+        if (customTip === null || customTip === void 0 ? void 0 : customTip.classList.contains("active")) {
             tipPercentage = parseFloat(customTip.value);
         }
         else {
             tipPercentage = parseFloat(buttonSelected[0].value); //defaultní hodnota
         }
     }
-    var billInt = parseFloat(bill.value);
-    var numberOfPeopleInt = parseInt(numberOfPeople.value);
-    var tipPerPerson = (billInt * tipPercentage * 0.01) / numberOfPeopleInt;
-    var totalPerPerson = (billInt / numberOfPeopleInt) + tipPerPerson;
-    console.log({ billInt: billInt, tipPerPerson: tipPerPerson, tipPercentage: tipPercentage });
-    resultTip.innerText = tipPerPerson.toFixed(2);
-    resultTotal.innerText = totalPerPerson.toFixed(2);
+    if (bill) {
+        var billInt = parseFloat(bill.value);
+        if (numberOfPeople) {
+            var numberOfPeopleInt = parseInt(numberOfPeople.value);
+            var tipPerPerson = (billInt * tipPercentage * 0.01) / numberOfPeopleInt;
+            var totalPerPerson = (billInt / numberOfPeopleInt) + tipPerPerson;
+            console.log({ billInt: billInt, tipPerPerson: tipPerPerson, tipPercentage: tipPercentage });
+            if (resultTip)
+                resultTip.innerText = tipPerPerson.toFixed(2);
+            if (resultTotal)
+                resultTotal.innerText = totalPerPerson.toFixed(2);
+        }
+    }
 }
 function calculateTip(button) {
     buttons.forEach(function (button) {
         button.classList.remove('active');
     });
     button.classList.add('active');
-    customTip.classList.remove('active');
+    customTip === null || customTip === void 0 ? void 0 : customTip.classList.remove('active');
     calculate();
 }
 function calculateCustomTip() {
     buttons.forEach(function (button) {
         button.classList.remove("active");
     });
-    customTip.classList.add("active");
-    if ((bill.value !== '' || parseFloat(bill.value) < 0) && (numberOfPeople.value !== '' || parseInt(numberOfPeople.value) > 0)) {
+    customTip === null || customTip === void 0 ? void 0 : customTip.classList.add("active");
+    if (((bill === null || bill === void 0 ? void 0 : bill.value) !== '' || parseFloat(bill.value) < 0) && ((numberOfPeople === null || numberOfPeople === void 0 ? void 0 : numberOfPeople.value) !== '' || parseInt(numberOfPeople.value) > 0)) {
         calculate();
     }
 }
 function dealWithResetButton() {
-    if (customTip.value === '' && bill.value === '' && numberOfPeople.value === '') {
-        reset.disabled = true;
-        reset.classList.remove('has-reset-activated');
+    if ((customTip === null || customTip === void 0 ? void 0 : customTip.value) === '' && (bill === null || bill === void 0 ? void 0 : bill.value) === '' && (numberOfPeople === null || numberOfPeople === void 0 ? void 0 : numberOfPeople.value) === '') {
+        if (reset)
+            reset.disabled = true;
+        reset === null || reset === void 0 ? void 0 : reset.classList.remove('has-reset-activated');
         numberOfPeople.style.borderColor = '';
     }
     else {
-        reset.disabled = false;
-        reset.classList.add('has-reset-activated');
+        if (reset)
+            reset.disabled = false;
+        reset === null || reset === void 0 ? void 0 : reset.classList.add('has-reset-activated');
     }
 }
 function resetAll() {
     buttons.forEach(function (button) {
         button.classList.remove("active");
     });
-    inputs.forEach(function (input) {
+    inputs === null || inputs === void 0 ? void 0 : inputs.forEach(function (input) {
         input.value = "";
     });
-    resultTip.innerText = "0.00";
-    resultTotal.innerText = "0.00";
-    reset.disabled = true;
-    errorMessage.innerText = "";
-    numberOfPeople.style.borderColor = '';
-    reset.classList.remove('has-reset-activated');
-    reset.style.backgroundColor = '';
-    // logniProKazdySudyCilo([10, 2, 1], logni) 
+    if (resultTip)
+        resultTip.innerText = "0.00";
+    if (resultTotal)
+        resultTotal.innerText = "0.00";
+    if (reset)
+        reset.disabled = true;
+    if (errorMessage)
+        errorMessage.innerText = "";
+    if (numberOfPeople)
+        numberOfPeople.style.borderColor = '';
+    if (reset) {
+        reset.classList.remove('has-reset-activated');
+        reset.style.backgroundColor = '';
+    }
 }
+// logniProKazdySudyCilo([10, 2, 1], logni) 
 // function logniProKazdySudyCislo(cisla, callback) {
 //     cisla.forEach((cislo) => {
 //         if(cislo % 2 === 0) {
